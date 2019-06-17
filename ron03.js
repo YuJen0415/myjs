d475f87e061d401e3d2190ac045bdb67b4083e2b
YuJen0415 <ss42199@hotmail.com>
2019-06-17 13:21:13 +0800

    0617_AM

---
+++ ron03.js
@@ -0,0 +1,23 @@
+let var1; 
+// 弱型別 可直接改變數型態
+document.write(var1 + "<br>");
+
+var1 = 123;
+document.write(typeof(var1) + "<br>");
+
+var1 = 12.3;
+document.write(typeof(var1) + "<br>");
+
+var1 = true;
+document.write(typeof(var1) + "<br>");
+
+var1 = "bryan";
+document.write(typeof(var1) + "<br>");
+
+// const需要先initial
+const name = 'Yeeeeeeeee'; 
+document.write(`My name is ${name}`);
+
+let a = 10;
+let b = 5;
+document.write(`${a} + ${b} = ${a+b}`);
