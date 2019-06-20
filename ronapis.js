e7ea27a06d1e6fa189f99e2d6b2e133523f8fe54
YuJen0415 <ss42199@hotmail.com>
2019-06-20 17:47:48 +0800

    0620_class

--- ronapis.js
+++ ronapis.js
@@ -91,3 +91,14 @@ function checkPrime(p) {
 
     return pStr;
 }
+
+function clone(source) {
+    if (typeof(source) != 'object') return null;
+    let target = new Object();
+    
+    for (let attr in source) {
+        target[attr] = source[attr];
+    }
+
+    return target;
+}
